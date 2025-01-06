22b12e2e96832400afa86213fb841cf71cbefecc
MariaOrozco28 <marshallk.2811@gmail.com>
2025-01-05 19:38:22 -0500

    Update index.jsx

--- src/componentes/footer/index.jsx
+++ src/componentes/footer/index.jsx
@@ -3,13 +3,13 @@ import "./footer.css";
 const Footer = () => {
     return <footer className="footer" style={{backgroundImage: "url(org/public/img/Footer.png)"}}>
         <div className="redes">
-            <a href="https://www.facebook.com/" target="_blank">
+            <a href="https://www.facebook.com/" target="blank">
                 <img src="./public/img/facebook.png" alt="Facebook"/>
             </a>
-            <a href="https://www.instagram.com/" target="_blank">
+            <a href="https://www.instagram.com/" target="blank">
                 <img src="./public/img/instagram.png" alt="Instagram"/>
             </a>
-            <a href="https://www.twitter.com/" target="_blank">
+            <a href="https://www.twitter.com/" target="blank">
                 <img src="./public/img/twitter.png" alt="Twitter"/>
             </a>
         </div>
