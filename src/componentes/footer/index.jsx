52f0230096d7ddcfce57aa0eacc47e26ebd1d518
MariaOrozco28 <marshallk.2811@gmail.com>
2025-01-05 19:40:51 -0500

    Update index.jsx

--- src/componentes/footer/index.jsx
+++ src/componentes/footer/index.jsx
@@ -1,22 +1,27 @@
 import "./footer.css";
 
 const Footer = () => {
-    return <footer className="footer" style={{backgroundImage: "url(/img/Footer.png)"}}>
-        <div className="redes">
-            <a href="https://www.facebook.com/" target="blank">
-                <img src="/img/facebook.png" alt="Facebook"/>
-            </a>
-            <a href="https://www.instagram.com/" target="blank">
-                <img src="/img/instagram.png" alt="Instagram"/>
-            </a>
-            <a href="https://www.twitter.com/" target="blank">
-                <img src="/img/twitter.png" alt="Twitter"/>
-            </a>
-        </div>
-        <img src="/img/Logo.png" alt="Logo_org"/>
-        <strong>Desarrollado por Mafer</strong>
-        
-    </footer>
-}
+    return (
+        <footer
+            className="footer"
+            style={{ backgroundImage: "url(/img/Footer.png)" }}
+        >
+            <div className="redes">
+                <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+                    <img src="/img/facebook.png" alt="Facebook" />
+                </a>
+                <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+                    <img src="/img/instagram.png" alt="Instagram" />
+                </a>
+                <a href="https://www.twitter.com/" target="_blank" rel="noopener noreferrer">
+                    <img src="/img/twitter.png" alt="Twitter" />
+                </a>
+            </div>
+            <img src="/img/Logo.png" alt="Logo_org" />
+            <strong>Desarrollado por Mafer</strong>
+        </footer>
+    );
+};
 
 export default Footer;
+
