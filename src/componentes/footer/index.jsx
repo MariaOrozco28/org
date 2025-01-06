e83f653c8ee06fe888d6ddd806625a1a319a5435
MariaOrozco28 <marshallk.2811@gmail.com>
2025-01-05 19:30:43 -0500

    cambio a las img

--- src/componentes/footer/index.jsx
+++ src/componentes/footer/index.jsx
@@ -1,19 +1,19 @@
 import "./footer.css";
 
 const Footer = () => {
-    return <footer className="footer" style={{backgroundImage: "url(org/build/img/Footer.png)"}}>
+    return <footer className="footer" style={{backgroundImage: "url(./org/public/img/Footer.png)"}}>
         <div className="redes">
             <a href="https://www.facebook.com/" target="_blank">
-                <img src="./img/facebook.png" alt="Facebook"/>
+                <img src="./org/public/img/facebook.png" alt="Facebook"/>
             </a>
             <a href="https://www.instagram.com/" target="_blank">
-                <img src="./img/instagram.png" alt="Instagram"/>
+                <img src="./org/public/img/instagram.png" alt="Instagram"/>
             </a>
             <a href="https://www.twitter.com/" target="_blank">
-                <img src="./img/twitter.png" alt="Twitter"/>
+                <img src="./org/public/img/twitter.png" alt="Twitter"/>
             </a>
         </div>
-        <img src="./img/Logo.png" alt="Logo_org"/>
+        <img src="./org/public/img/Logo.png" alt="Logo_org"/>
         <strong>Desarrollado por Mafer</strong>
         
     </footer>
